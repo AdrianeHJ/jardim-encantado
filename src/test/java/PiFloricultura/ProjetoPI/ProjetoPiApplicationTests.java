bc6021656c98d168b0bf02ac4ed770bae06d45b0
ADRIANE HELFSTEIN DE JESUS <adriane.hjesus@senacsp.edu.br>
2025-10-28 20:52:39 -0300

    Primeira apresentação do PI

---
+++ src/test/java/PiFloricultura/ProjetoPI/ProjetoPiApplicationTests.java
@@ -0,0 +1,13 @@
+package PiFloricultura.ProjetoPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
